2f249f1aab44c7ee92f027f4842135e4dc6055fb
karboosx <karbo243@gmail.com>
2017-05-05 10:48:20 +0200

    hot update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import TrueButton from './testClass/TrueButton'
 
 (function () {
     let trueButton = new TrueButton();
-    console.log('button');
+
     document.getElementById('app').appendChild(trueButton.render());
 
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ var webpack = require('webpack');
 module.exports = {
     entry: './main.js',
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/assets/",
+        filename: "bundle.js"
     },
     module: {
         loaders: [
